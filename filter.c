5eb6049173c8af277cd117ef38a32c7803417688
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-04-16 20:55:28 +0530

    Updated numbers

--- filter.c
+++ filter.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int numbers[5] = {1, 2, 3, 4, 5};
+    int numbers[] = {1, 2, 3, 4, 5, 34, 34, 24, 25, 3, 5, 34, 2, 313, 23, 32, 34, 23};
     int numberOfEvens = 0;
     int count_of_numbers = sizeof(numbers) / sizeof(numbers[0]);
     for (int i = 0; i < count_of_numbers; i++)
